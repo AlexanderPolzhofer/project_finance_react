e092ad9c8c39b4abd3a32d20ce329a3b27fc3f3d
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2022-01-11 22:06:12 +0100

    adding date function in  task component

--- src/components/task/Task.component.js
+++ src/components/task/Task.component.js
@@ -4,9 +4,15 @@ import RemoveIcon from '../remove-icon/RemoveIcon.component';
 import './Task.styles.css';
 
 const Task = ({ stockName, notes }) => {
+    
+    const getDate = () => {
+        const today = new Date();
+        return today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    }
+
     return (
         <div className='task-container'>
-            <div className='date'>DATUM</div>
+            <div className='date'>{getDate()}</div>
             <div className='stock'>{stockName}</div>
             <div className='removeIcon'><RemoveIcon /></div>
             <div className='notes'>{notes}</div>
