5de38be0bdf8944d6ebf941f4d45b8b2f1a65545
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2022-01-13 21:30:47 +0100

    fetching technical data from alpha vantage

--- src/pages/Indexscreen/IndexScreen.js
+++ src/pages/Indexscreen/IndexScreen.js
@@ -16,6 +16,7 @@ export default function IndexScreen() {
     let nav = useNavigate();
     const handleOnListItemClick = (value) => {
         nav('/indexDetails', { state: value });
+        console.log(value)
     }
 
     useEffect(() => {
--- src/pages/StockDetail/StockDetail.js
+++ src/pages/StockDetail/StockDetail.js
@@ -16,11 +16,18 @@ export default function StockDetail() {
     const [stockDetail, setStockDetail] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    const [technicalData, setTechnicalData] = useState([]);
+    const [isTechnicalDataLoading, setIstTechnicalDataLoading] = useState(true);
+
 
     useEffect(() => {
         const fetchingData = async () => {
             /*const url = `https://api.leeway.tech/api/v1/public/fundamentals/${state.symbol}.XETRA?apitoken=${apiKey}`;*/
+
+            const apiKeyAlphaVantage = 'RFPSJQBOAQINO7QV';
+
             const url = `http://localhost:8080/stock/value/${state.symbol}.XETRA`;
+            const alphaVantageUrl = `https://www.alphavantage.co/query?function=MAMA&symbol=${state.symbol}&interval=daily&series_type=close&fastlimit=0.02&apikey=${apiKeyAlphaVantage}`;
 
             try {
                 const response = await fetch(url);
@@ -29,6 +36,16 @@ export default function StockDetail() {
                 setIsLoading(false)
             } catch (error) {
                 console.log('ERROR: ' + error)
+            }
+
+            try {
+                const res = await fetch(alphaVantageUrl);
+                const technicalDataFromApi = await res.json();
+                //setTechnicalData(technicalDataFromApi);
+                //setIstTechnicalDataLoading(false);
+                console.log(technicalDataFromApi);
+            } catch (err) {
+                console.log('ERROR alpha vantage api: ' + err.stack)
             }
         }
         fetchingData();
